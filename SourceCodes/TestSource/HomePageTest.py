67eba5a7d4aa29db7edeb2b27a29278e9a5d2e30
deepak thorecha <dthorecha@ebay.com>
2017-12-12 18:35:09 +0530

    Trying Data-driven test

--- SourceCodes/TestSource/HomePageTest.py
+++ SourceCodes/TestSource/HomePageTest.py
@@ -2,24 +2,32 @@ from SourceCodes.BaseUtilities.Base import Base
 from SourceCodes.PageClasses.HomePage import HomePage
 from SourceCodes.PageClasses.SignInPage import SignInPage
 import unittest
+import pandas as pd
+from ddt import ddt, data
 
 
+#@ddt
 class HomePageTest (unittest.TestCase):
 
     def setUp(self):
         self.base = Base()
         self.driver = self.base.getChromeDriver()
 
-    def test_checkCurrentURL(self):
+    #@data(pd.DataFrame("/ResourceFiles/ProdUserDetails.xls").iterrows())
+    def check_LoginUser(self, test_DATA):
         assert self.driver.current_url == "https://www.ebay.com.au/"
-
-    def test_LoginUser(self):
         home = HomePage(self.driver)
         home.clickSignIn()
         signIn = SignInPage(self.driver)
-        signIn.enterUserName("testaubasic")
-        signIn.enterPassword("N0wBuy1t!")
+        signIn.enterUserName(test_DATA['ProdUserData.userName'])
+        signIn.enterPassword(test_DATA['ProdUserData.password'])
         signIn.clickSignInButton()
+
+    def test_Login(self):
+        self.AU_DATA = pd.read_excel("/Users/dthorecha/PycharmProjects/PySeleniumProject/ResourceFiles/ProdUserDetails.xls")
+        self.AU_DATA = self.AU_DATA[self.AU_DATA.get('TestObject.TestSite') == 'AU']
+        for row in self.AU_DATA.iterrows():
+            self.check_LoginUser(row)
 
     def tearDown(self):
         self.driver.close()
